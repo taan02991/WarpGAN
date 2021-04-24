b3595f0ae9e3492e8c93496b03f4ca9df0aaf481
Thanawat <taan02991@hotmail.com>
2021-04-24 23:11:00 +0700

    feat(encode): return encode base64

--- api/caricature.py
+++ api/caricature.py
@@ -4,6 +4,7 @@ from starlette.responses import StreamingResponse
 import io
 import cv2
 import imageio
+import base64
 
 
 router = APIRouter()
@@ -16,8 +17,8 @@ async def upload_file(file: UploadFile = File(...)):
     img_result = transform_face(img)
     img_result = cv2.cvtColor(img_result, cv2.COLOR_BGR2RGB)
     _, im_png = cv2.imencode(".png", img_result * 255)
-
-    return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
+    base64_img = base64.b64encode(im_png)
+    return {"data": base64_img}
 
 
 @router.get("/test")
